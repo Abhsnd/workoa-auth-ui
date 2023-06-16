d2e808ac9fb8035773486d75932b738615b68fef
wiho <wiho_ncoder@163.com>
2023-06-17 00:01:09 +0800

    finsh

---
+++ src/api/wechat/menu.js
@@ -0,0 +1,58 @@
+import request from '@/utils/request'
+
+const api_name = '/admin/wechat/menu'
+
+export default {
+    // 同步菜单
+    syncMenu() {
+        return request({
+            url: `${api_name}/syncMenu`,
+            method: `get`
+        })
+    },
+
+    // 删除菜单
+    removeMenu() {
+        return request({
+            url: `${api_name}/removeMenu`,
+            method: `delete`
+        })
+    },
+
+    findMenuInfo() {
+        return request({
+            url: `${api_name}/findMenuInfo`,
+            method: `get`
+        })
+    },
+    
+    save(menu) {
+        return request({
+            url: `${api_name}/save`,
+            method: `post`,
+            data: menu
+        })
+    },
+    
+    getById(id) {
+        return request({
+            url: `${api_name}/get/${id}`,
+            method: `get`
+        })
+    },
+    
+    updateById(menu) {
+        return request({
+            url: `${api_name}/update`,
+            method: `put`,
+            data: menu
+        })
+    },
+    
+    removeById(id) {
+        return request({
+            url: `${api_name}/remove/${id}`,
+            method: 'delete'
+        })
+    }
+}
